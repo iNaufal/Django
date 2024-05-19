f5b51f42e6dbb5af7a5f645a29bd7d6d1e52dfc6
Veno veno <naufalpriatna26@gmail.com>
2024-05-19 21:23:56 +0700

    fea(TailwindCSS): Fix the tailwindCSS Installation

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def index(request):
   context = {
-    'title': 'Web development full stack',
+    'title': 'Try your best and do what you need to do, while you still have the time - Venomare',
     'subtitle': 'Welcome to my website',
     'developer': 'Venomare',
     'banner': 'images/vreedom.jpeg',
--- mysite/static/tailwind.config.js
+++ mysite/static/tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: [
     '../**/templates/*.html',
-    '../**/templates/**/*.html'
+    '../**/templates/**/*.html',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
