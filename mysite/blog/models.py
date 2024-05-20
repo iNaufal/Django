0f5762bba009c4c1ea74b93e1bdbfe96d506436a
Veno veno <naufalpriatna26@gmail.com>
2024-05-20 17:03:33 +0700

    Fea(PostgreSQL, Models, Admin): connect django project to PostgreSQL, setting up models, register apps to admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+  title = models.CharField(max_length=256) #atribute dari package model
+  fill = models.TextField(null=True)
+  OPTION_CATEGORY = [
+    ('python', 'python'),
+    ('django', 'django'),
+    ('javascript', 'javascript'),
+  ]
+  categories = models.CharField(max_length=50, choices=OPTION_CATEGORY)
+  
+  def __str__(self):
+    return self.title
