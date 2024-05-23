8a9702ef7867b236683b8d527d2e282373cbc542
Veno veno <naufalpriatna26@gmail.com>
2024-05-23 21:18:16 +0700

    fea(db relation): database relationships 1

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+  name = models.CharField(max_length=50)
+  
+  def __str__(self):
+    return self.name
+
+class Reader(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(blank=True)
+  
+  def __str__(self):
+    return self.name
+
+class Tag(models.Model):
+  name = models.CharField(max_length=50)
+  
+  def __str__(self):
+    return self.name
+
 class Article(models.Model):
   title = models.CharField(max_length=256) #atribute dari package model
   fill = models.TextField(null=True)
@@ -10,6 +29,12 @@ class Article(models.Model):
     ('javascript', 'javascript'),
   ]
   categories = models.CharField(max_length=50, choices=OPTION_CATEGORY)
+  publications = models.OneToOneField(Publication, on_delete=models.CASCADE)
+  tags = models.ManyToManyField(Tag)
   
   def __str__(self):
     return self.title
+  
+class ReadList(models.Model):
+  readers = models.ForeignKey(Reader, on_delete=models.CASCADE)
+  articles = models.ForeignKey(Article, on_delete=models.CASCADE)
